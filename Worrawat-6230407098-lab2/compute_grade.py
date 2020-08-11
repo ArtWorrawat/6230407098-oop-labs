ba6749c2cf3d900770767c06b83e6f9132a092cf
ArtWorrawat <69181053+ArtWorrawat@users.noreply.github.com>
2020-08-11 10:42:05 +0700

    Add files via upload

--- Worrawat-6230407098-lab2/compute_grade.py
+++ Worrawat-6230407098-lab2/compute_grade.py
@@ -1,22 +1,41 @@
-def check(round) :
-    if round == "1":
-        term = "midterm"
-    elif round == "2":
-        term = "final"
-    while True:
+def check(round):
+    if round == 0:
+        while True:
+            value = input("Please enter the student's midterm exam mark (0-100):")
+            try:
+                value = float(value)
+                if 100 >= value >= 0:
+                    return value
+                else:
+                    print("Please enter a valid exam mark (0-100)")
+            except:
+                print("Please enter a valid exam mark (0-100)")
+    elif round == 1:
+        while True:
+            value = input("Please enter the student's final exam mark (0-100):")
+            try:
+                value = float(value)
+                if 100 >= value >= 0:
+                    return value
+                else:
+                    print("Please enter a valid exam mark (0-100)")
+            except:
+                print("Please enter a valid exam mark (0-100)")
 
+
+score = 0
 name = input("Please enter a student name: ")
-for i in  range(2):
+for i in range(2):
     score__ex = check(i)
-
+    score += score__ex
 score /= 2
 if score < 50:
-    print("%s has total mark as"%name, "%.2f and grade as F"%score)
+    print("%s has total mark as" % name, "%.2f and grade as F" % score)
 elif score < 60:
-    print("%s has total mark as"%name, "%.2f and grade as D"%score)
+    print("%s has total mark as" % name, "%.2f and grade as D" % score)
 elif score < 70:
-    print("%s has total mark as"%name, "%.2f and grade as C"%score)
+    print("%s has total mark as" % name, "%.2f and grade as C" % score)
 elif score < 80:
-    print("%s has total mark as"%name, "%.2f and grade as B"%score)
+    print("%s has total mark as" % name, "%.2f and grade as B" % score)
 else:
-    print("%s has total mark as"%name, "%.2f and grade as A"%score)
+    print("%s has total mark as" % name, "%.2f and grade as A" % score)
