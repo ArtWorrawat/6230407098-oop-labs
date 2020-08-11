72c16c82d05dbb11fefedd1b35df6e0bac1c8a56
ArtWorrawat <69181053+ArtWorrawat@users.noreply.github.com>
2020-08-11 09:41:58 +0700

    Delete convert_temperature.py

--- Worrawat-6230407098-lab2/convert_temperature.py
+++
@@ -1,9 +0,0 @@
-while True:
-    t_f = input("Enter a temperature in Fahrenheit: ")
-    try:
-        t_f = float(t_f)
-        break
-    except:
-        print("Please enter a valid floating point for the temperature")
-t_c = (5 * (t_f - 32)) / 9
-print("Temperature %.2f in Fahrenheit is" %t_f, "%.2f in Celcius"%t_c)
