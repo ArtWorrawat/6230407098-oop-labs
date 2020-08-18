dee16c872bd595caf028341ce75fc395856e3cf5
ArtWorrawat <69181053+ArtWorrawat@users.noreply.github.com>
2020-08-18 11:39:07 +0700

    Update P4.py

--- Worrawat-6230407098-lab3/P4.py
+++ Worrawat-6230407098-lab3/P4.py
@@ -1,6 +1,10 @@
 number = int(input("Enter a number to find the factorial:"))
 summary = 1
 
-for i in range(number):
-    summary *= i+1
-print(f"Factorial of {number} is {summary}")
+if number == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(number):
+        summary *= i+1
+    print(f"Factorial of {number} is {summary}")
+
