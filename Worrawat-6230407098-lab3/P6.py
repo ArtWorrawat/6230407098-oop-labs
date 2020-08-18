6406c8b6c9cf3b5f5478649df75e457666e10dfb
ArtWorrawat <69181053+ArtWorrawat@users.noreply.github.com>
2020-08-18 10:11:59 +0700

    Update P6.py

--- Worrawat-6230407098-lab3/P6.py
+++ Worrawat-6230407098-lab3/P6.py
@@ -4,5 +4,6 @@ dates = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 dict_items = {}
 for i in range(12):
     dict_items[months[i]] = dates[i]
+print (dict_items)
 month_select = input("Enter month:")
 print(f"Number of days in {month_select} is {dict_items[month_select]}")
