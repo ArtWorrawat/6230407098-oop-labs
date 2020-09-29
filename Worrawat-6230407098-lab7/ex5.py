c7a7c607ab63ff88c2c81a88f20cdfa57d5e5b8c
ArtWorrawat <69181053+ArtWorrawat@users.noreply.github.com>
2020-09-29 11:30:22 +0700

    Update ex5.py

--- Worrawat-6230407098-lab7/ex5.py
+++ Worrawat-6230407098-lab7/ex5.py
@@ -12,7 +12,12 @@ class Circle:
         return 2 * math.pi * self.radius
 
 
-print("The circle with radius 3 has the area as {:.2f} and the perimeter as {:.2f}".format(Circle(3).get_area(),
-                                                                                         Circle(3).get_perimeter()))
-print("The circle with radius 4 has the area as {:.2f} and the perimeter as {:.2f}".format(Circle(4).get_area(),
-                                                                                         Circle(4).get_perimeter()))
+if __name__ == '__main__':
+    radius1 = 3
+    radius2 = 4
+    print("The circle with radius 3 has the area as {:.2f} and the perimeter as {:.2f}".format(Circle(radius1).get_area(),
+                                                                                               Circle(
+                                                                                                   radius1).get_perimeter()))
+    print("The circle with radius 4 has the area as {:.2f} and the perimeter as {:.2f}".format(Circle(radius2).get_area(),
+                                                                                           Circle(radius2).get_perimeter()))
+
